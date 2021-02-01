3790302096d05727d44cae54a11fcfab46b81563
i.konkin@it-alnc.ru <9IrY1*^Q4$5x>
2021-02-02 00:14:21 +0300

    Добавлено получение списка задач

--- main.py
+++ main.py
@@ -50,6 +50,11 @@ class Project:
                               for i in kwargs.get('custom_fields')}
         self.users = {}
 
+    def __repr__(self):
+        return f'Project: {self.name}\n' \
+               f'   id: {self.id}\n' \
+               f'   identifier: {self.identifier}\n'
+
     def get_users(self):
         resp = req.get(Config.REDMINE_URL+f'/projects/{self.id}/memberships.json',
                        auth=HTTPBasicAuth(Config.LOGIN, Config.PASSWORD))
@@ -89,10 +94,33 @@ class User:
                        auth=HTTPBasicAuth(Config.LOGIN, Config.PASSWORD),
                        params={'assigned_to_id': self.id}
                        )
+        self.tasks = {}
+        for i in resp.json()['issues']:
+            self.tasks[i['id']] = Task(id=i['id'],
+                                       project=i.get('project'),
+                                       tracker=i.get('tracker'),
+                                       status=i.get('status'),
+                                       priority=i.get('priority'),
+                                       author=i.get('author'),
+                                       assigned_to=i.get('assigned_to'),
+                                       parent=i.get('parent'),
+                                       subject=i.get('subject'),
+                                       description=i.get('description'),
+                                       start_date=i.get('start_date'),
+                                       due_date=i.get('due_date'),
+                                       done_ratio=i.get('done_ratio'),
+                                       is_private=i.get('is_private'),
+                                       estimated_hours=i.get('estimated_hours'),
+                                       custom_fields=i.get('custom_fields'),
+                                       created_on=i.get('created_on'),
+                                       updated_on=i.get('updated_on'),
+                                       closed_on=i.get('closed_on')
+                                       )
 
 
 class Task:
     def __init__(self, **kwargs):
+        # print(f'debug id = {kwargs.get("id")}')
         self.id = kwargs.get('id')
         self.project = kwargs.get('project')
         self.tracker = kwargs.get('tracker')
@@ -109,11 +137,19 @@ class Task:
         self.is_private = kwargs.get('is_private')
         self.estimated_hours = kwargs.get('estimated_hours')
         self.custom_fields = kwargs.get('custom_fields')
-        self.id = kwargs.get('created_on')
-        self.id = kwargs.get('updated_on')
-        self.id = kwargs.get('closed_on')
 
+    def __repr__(self):
+        # print(f'debug repr id = {self.id}')
+        return f'Task #{self.id}: subject: {self.subject}'
 
 test = Projects()
+test['mortgage'].get_users()
+cur = test['mortgage'].users['i.konkin']
+"""
+:type cur: User
+"""
+cur.get_open_tasks()
+print(cur.tasks)
+
 # test['mortgage'].get_users()
-print(User(name='i.konkin'))
+# print(User(name='i.konkin'))
